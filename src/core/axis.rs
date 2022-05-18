f7c8340947018df97f3fe9c64430ea5edaf27fde
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-05-18 19:22:07 -0400

    Added a logspace function

--- src/core/axis.rs
+++ src/core/axis.rs
@@ -1,8 +1,20 @@
+pub const DEFAULT_SPACE_LEN: i32 = 50;
+pub const DEFAULT_LOGSPACE_BASE: f32 = 10.0;
+
 pub fn linspace_f32(start: f32, end: f32, num: i32) -> Vec<f32> {
     let delta = (end - start) / ((num as f32) - 1.0);
     let mut result = vec![];
     for i in 0..num {
         result.push(start + (i as f32) * delta);
+    }
+    return result;
+}
+
+pub fn logspace_f32(start: f32, end: f32, num: i32, base: f32) -> Vec<f32> {
+    let exponents = linspace_f32(start, end, num);
+    let mut result = vec![];
+    for i in exponents {
+        result.push(base.powf(i));
     }
     return result;
 }
@@ -28,12 +40,37 @@ pub fn arange_f32(start: f32, end: f32, step: f32) -> Vec<f32> {
 
 #[macro_export]
 macro_rules! linspace {
-    ($x:expr, $y:expr, $z:expr) => {{
-        assert_eq!(true, rom_rs::utils::is_int($z as f32));
-        rom_rs::linspace_f32($x as f32, $y as f32, $z as i32)
+    ($start:expr, $end:expr, $num:expr) => {{
+        assert_eq!(true, rom_rs::utils::is_int($num as f32));
+        rom_rs::linspace_f32($start as f32, $end as f32, $num as i32)
     }};
-    ($x:expr, $y:expr) => {
-        linspace_f32($x as f32, $y as f32, 50)
+    ($start:expr, $end:expr) => {
+        linspace_f32($start as f32, $end as f32, DEFAULT_SPACE_LEN)
+    };
+}
+
+#[macro_export]
+macro_rules! logspace {
+    ($start:expr, $end:expr, $num:expr, $base:expr) => {{
+        assert_eq!(true, rom_rs::utils::is_int($num as f32));
+        rom_rs::logspace_f32($start as f32, $end as f32, $num as i32, $base as f32)
+    }};
+    ($start:expr, $end:expr, $num:expr) => {{
+        assert_eq!(true, rom_rs::utils::is_int($num as f32));
+        rom_rs::logspace_f32(
+            $start as f32,
+            $end as f32,
+            $num as i32,
+            rom_rs::axis::DEFAULT_LOGSPACE_BASE,
+        )
+    }};
+    ($start:expr, $end:expr) => {
+        logspace_f32(
+            $start as f32,
+            $end as f32,
+            rom_rs::axis::DEFAULT_SPACE_LEN,
+            rom_rs::axis::DEFAULT_LOGSPACE_BASE,
+        )
     };
 }
 
--- tests/core/axis.rs
+++ tests/core/axis.rs
@@ -1,4 +1,4 @@
-use rom_rs::{arange, linspace};
+use rom_rs::{arange, linspace, logspace};
 
 #[test]
 fn axis_linspace() {
@@ -14,6 +14,13 @@ fn axis_linspace() {
         vec![0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0],
         linspace!(0.0, 10.0, 11),
     );
+}
+
+#[test]
+fn axis_logspace() {
+    assert_eq!(vec![1.0, 10.0, 100.0, 1000.0], logspace!(0, 3, 4));
+    assert_eq!(vec![2.0, 4.0, 8.0], logspace!(1, 3, 3, 2));
+    assert_eq!(vec![9.0, 27.0, 81.0, 243.0], logspace!(2, 5, 4, 3));
 }
 
 #[test]
