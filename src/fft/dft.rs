2ffc4c245067b4f243fdbbd575bc8ab0c6f4a101
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-05-19 16:03:50 -0400

    Added a first implementation of the DFT

--- src/fft/dft.rs
+++ src/fft/dft.rs
@@ -1,7 +1,40 @@
-// pub fn dft1(values: Vec<f32>) -> Vec<f32> {
+use crate::{Number, Complex, PI};
+
+pub fn dft1<T: Number>(values: Vec<T>) -> Vec<Complex> {
+    let length = values.len() as i32;
+    let mut result = Vec::<Complex>::new();
+    for k in 0..length {
+        let mut temp = Complex::zero();
+        for n in 0..length {
+            temp += values[n as usize].to_complex() * (-Complex::j() * 2 * n * k * PI / length);
+        }
+        result.push(temp);
+    }
+    return result;
+}
+
+// pub fn dft1_f32(values: Vec<f32>) -> Vec<Complex> {
 //     let length = values.len() as i32;
-//     let mut result = Vec::new();
+//     let mut result = Vec::<Complex>::new();
 //     for k in 0..length {
-//         let temp = 
+//         let mut temp = Complex::zero();
+//         for n in 0..length {
+//             temp += values[n as usize].to_complex() * (-Complex::j() * 2 * n * k * PI / length).exp();
+//         }
+//         result.push(temp);
 //     }
+//     return result;
+// }
+
+// pub fn dft1_complex(values: Vec<Complex>) -> Vec<Complex> {
+//     let length = values.len() as i32;
+//     let mut result = Vec::<Complex>::new();
+//     for k in 0..length {
+//         let mut temp = Complex::zero();
+//         for n in 0..length {
+//             temp += values[n as usize] * (-Complex::j() * 2 * n * k * PI / length);
+//         }
+//         result.push(temp);
+//     }
+//     return result;
 // }
