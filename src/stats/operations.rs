fb46360dc231e30fbaecbbe472243783937c76a1
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-05-18 20:09:37 -0400

    Implemented more operations

--- src/stats/operations.rs
+++ src/stats/operations.rs
@@ -1,4 +1,6 @@
-pub fn meanf(vals: Vec<f32>) -> f32 {
+use crate::Complex;
+
+pub fn mean_f32(vals: Vec<f32>) -> f32 {
     let vals_len = vals.len() as f32;
     let mut result = 0.0;
     for i in vals {
@@ -7,11 +9,51 @@ pub fn meanf(vals: Vec<f32>) -> f32 {
     return result / vals_len;
 }
 
-pub fn meani(vals: Vec<i32>) -> f32 {
+pub fn mean_complex(vals: Vec<Complex>) -> Complex {
     let vals_len = vals.len() as f32;
-    let mut result = 0.0;
+    let mut result = Complex::zero();
     for i in vals {
-        result += i as f32;
+        result += i;
     }
     return result / vals_len;
+}
+
+pub fn median_f32(vals: Vec<f32>) -> f32 {
+    let mut vals_copy = vals.clone();
+    vals_copy.sort_by(|a, b| a.partial_cmp(b).unwrap());
+    let vals_len = vals_copy.len() as i32;
+
+    if vals_len % 2 == 0 {
+        let i_high = (vals_len / 2) as usize;
+        let i_low = i_high - 1;
+        return (vals_copy[i_high] - vals_copy[i_low]) / 2.0;
+    } else {
+        return vals_copy[((vals_len as f32) / 2.0).floor() as usize];
+    }
+}
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+//======================================|| Macros ||=============================================//
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+#[macro_export]
+macro_rules! mean {
+    ($($x:expr),*) => {{
+        let mut temp_vec = Vec::new();
+        $(
+            temp_vec.push($x as f32);
+        )*
+        rom_rs::stats::mean_f32(temp_vec)
+    }};
+}
+
+#[macro_export]
+macro_rules! median {
+    ($($x:expr),*) => {{
+        let mut temp_vec = Vec::new();
+        $(
+            temp_vec.push($x as f32);
+        )*
+        rom_rs::stats::median_f32(temp_vec)
+    }};
 }
