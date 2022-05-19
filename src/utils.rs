dbb178ebf5919fbef721d7ee07964301933053d9
Cristóbal Eduardo Allendes Molina <callendes.molina@gmail.com>
2022-05-18 20:27:11 -0400

    Added a method to convert an i32 vector to an f32 vector

--- src/utils.rs
+++ src/utils.rs
@@ -1,3 +1,11 @@
 pub fn is_int(num: f32) -> bool {
     return num == num.floor();
 }
+
+pub fn i32_to_f32(vals: Vec<i32>) -> Vec<f32> {
+    let mut result = Vec::<f32>::new();
+    for i in vals {
+        result.push(i as f32);
+    }
+    return result;
+}
